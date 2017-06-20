aacd85c71a123c3bf0932d9f44b996d8ae5bdc52
AkiFuj <tessitetsusi@gmail.com>
2017-06-20 10:42:43 +0900

    add initializer to Repository model

--- GitHubSearchRepository/Repository.swift
+++ GitHubSearchRepository/Repository.swift
@@ -6,9 +6,19 @@
 //  Copyright © 2017年 Akifumi Fujita. All rights reserved.
 //
 
+import Foundation
+import SwiftyJSON
+
 struct Repository {
     let id: Int
     let name: String
     let fullname: String
     let owner: User
+    
+    init(json: JSON) {
+        self.id = json["id"].intValue
+        self.name = json["name"].stringValue
+        self.fullname = json["fullname"].stringValue
+        self.owner = User(json: json["owner"])
+    }
 }
