e88175fc75ad6f390c6f49392dd0ebefbdbf53c5
AkiFuj <tessitetsusi@gmail.com>
2017-06-20 14:52:07 +0900

    add UIProgressView to toolbar of WebVC

--- GitHubSearchRepository/WebViewController.swift
+++ GitHubSearchRepository/WebViewController.swift
@@ -13,6 +13,7 @@ class WebViewController: UITableViewController, WKNavigationDelegate {
     
     var webView: WKWebView!
     var html_url: String!
+    var progressView: UIProgressView!
     
     override func loadView() {
         webView = WKWebView()
@@ -25,8 +26,28 @@ class WebViewController: UITableViewController, WKNavigationDelegate {
         let url = URL(string: html_url)!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
+        
+        progressView = UIProgressView(progressViewStyle: .default)
+        progressView.sizeToFit()
+        let progressButton = UIBarButtonItem(customView: progressView)
+        toolbarItems = [progressButton]
+        navigationController?.isToolbarHidden = false
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
     }
     
+    deinit {
+        webView!.removeObserver(self, forKeyPath: "estimatedProgress")
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "estimatedProgress" {
+            progressView.progress = Float(webView.estimatedProgress)
+        }
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
