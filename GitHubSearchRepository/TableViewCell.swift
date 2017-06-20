d1bd719409dfbb54630a414cbebe4d5f91ee19b5
AkiFuj <tessitetsusi@gmail.com>
2017-06-20 13:35:48 +0900

    create cell and connect it to storyboard

---
+++ GitHubSearchRepository/TableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  TableViewCell.swift
+//  GitHubSearchRepository
+//
+//  Created by 藤田哲史 on 2017/06/20.
+//  Copyright © 2017年 Akifumi Fujita. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var fullnameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var languageLabel: UILabel!
+    
+    let url: String? = nil
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
