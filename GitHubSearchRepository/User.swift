fc569d2c6c394c6a953d01aa69598e7ce4aa3849
AkiFuj <tessitetsusi@gmail.com>
2017-06-20 10:40:55 +0900

    add initializer to User model

--- GitHubSearchRepository/User.swift
+++ GitHubSearchRepository/User.swift
@@ -6,7 +6,15 @@
 //  Copyright © 2017年 Akifumi Fujita. All rights reserved.
 //
 
+import Foundation
+import SwiftyJSON
+
 struct User {
     let id: Int
     let login: String
+    
+    init(json: JSON) {
+        self.id = json["id"].intValue
+        self.login = json["login"].stringValue
+    }
 }
