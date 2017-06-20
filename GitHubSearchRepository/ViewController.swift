208996d1c0f929bc18b907c46ed1e0d239e8ebd1
AkiFuj <tessitetsusi@gmail.com>
2017-06-20 12:40:28 +0900

    create searchBar and search function

--- GitHubSearchRepository/ViewController.swift
+++ GitHubSearchRepository/ViewController.swift
@@ -7,19 +7,45 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController, UISearchBarDelegate {
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        searchBar.delegate = self
     }
+    
+    func searchBar(_ searchBar: UISearchBar, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        let deadline = DispatchTime.now() + .seconds(1)
+        DispatchQueue.main.asyncAfter(deadline: deadline) {
+            self.searchBySearchBarText()
+        }
+        return true
+    }
+    
+    func searchBySearchBarText() {
+        if let searchText = searchBar.text {
+            Alamofire.request(Router.search(q: searchText, sort: "", order: "")).responseJSON { [unowned self] response in
+                switch response.result {
+                case .success:
+                    let json = JSON(response.data)
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
+        }
+    }
+    
+    // 検索ワードを受け取ってサーバ通信 or ローカルのデータを絞り込む処理を行う
+    func search(text: String) {}
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
 
 }
 
