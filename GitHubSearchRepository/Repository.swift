11d79a998e79728263f2ce0828a38e62e4a6f99b
AkiFuj <tessitetsusi@gmail.com>
2017-06-20 10:25:44 +0900

    add Repository type

---
+++ GitHubSearchRepository/Repository.swift
@@ -0,0 +1,14 @@
+//
+//  Repository.swift
+//  GitHubSearchRepository
+//
+//  Created by 藤田哲史 on 2017/06/20.
+//  Copyright © 2017年 Akifumi Fujita. All rights reserved.
+//
+
+struct Repository {
+    let id: Int
+    let name: String
+    let fullname: String
+    let owner: User
+}
