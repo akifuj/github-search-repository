24182d8444676c6819269f8e30f081a96e9dd210
AkiFuj <tessitetsusi@gmail.com>
2017-06-20 10:24:25 +0900

    add User type

---
+++ GitHubSearchRepository/User.swift
@@ -0,0 +1,12 @@
+//
+//  User.swift
+//  GitHubSearchRepository
+//
+//  Created by 藤田哲史 on 2017/06/20.
+//  Copyright © 2017年 Akifumi Fujita. All rights reserved.
+//
+
+struct User {
+    let id: Int
+    let login: String
+}
