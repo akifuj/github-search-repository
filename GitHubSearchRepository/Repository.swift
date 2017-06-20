73ac6370b3d9d1614ed64975af903476331b12cb
AkiFuj <tessitetsusi@gmail.com>
2017-06-21 07:45:59 +0900

    Delete User model and unnecessary properties of Repository model

--- GitHubSearchRepository/Repository.swift
+++ GitHubSearchRepository/Repository.swift
@@ -10,21 +10,15 @@ import Foundation
 import SwiftyJSON
 
 struct Repository {
-    let id: Int
-    let name: String
     let fullname: String
-    let owner: User
     let description: String
     let language: String
     let html_url: String
     
     init(json: JSON) {
-        self.id = json["id"].intValue
-        self.name = json["name"].stringValue
         self.fullname = json["full_name"].stringValue
         self.description = json["description"].stringValue
         self.language = json["language"].stringValue
         self.html_url = json["html_url"].stringValue
-        self.owner = User(json: json["owner"])
     }
 }
